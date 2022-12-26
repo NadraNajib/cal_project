0f0e43c157f7076e7e12a01ca2f8d86b719755d7
NadraNajib <121390664+NadraNajib@users.noreply.github.com>
2022-12-26 16:27:39 +0100

    Add files via upload

---
+++ NadrasCalculator.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.nadrascalculator;
+
+/**
+ *
+ * @author nadra
+ */
+public class NadrasCalculator {
+
+    public static void main(String[] args) {
+        JFrame gui = new JFrame();
+        //gui.pack();
+        gui.setVisible(true);
+    }
+}
